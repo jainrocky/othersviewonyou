3a8d67045e30129f8c7aff4b8335cbaad9d4f7f2
rockyjain <jainrocky1008@gmail.com>
2024-12-15 23:27:29 +0530

    removed UserNameQuerySchema from check-username api

--- src/app/api/check-username/route.ts
+++ src/app/api/check-username/route.ts
@@ -5,18 +5,18 @@ import { usernameValidation } from "@/schemas/signUpSchema";
 import { NextRequest } from "next/server";
 import { z } from "zod";
 
-export const UserNameQuerySchema=z.object({
-    username: usernameValidation
-})
+// export const UserNameQuerySchema=z.object({
+//     username: usernameValidation
+// })
 
 //No authorization required for this api
 export async function GET(request: NextRequest){
     try{
         const {searchParams}= new URL(request.url)
-        const queryParams = {
-            username:searchParams.get('username'),
-        }
-        const {username}=UserNameQuerySchema.parse(queryParams)
+        // const queryParams = {
+        //     username:searchParams.get('username'),
+        // }
+        const username=searchParams.get('username')
 
         await dbConnect()
         const user = await UserModel.findOne({username:username, isVerified:true})
