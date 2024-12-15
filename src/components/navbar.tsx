8db0a2f9b517c2ebdb4aaf0d33df7a250dbeaa77
rockyjain <jainrocky1008@gmail.com>
2024-12-15 23:39:25 +0530

    added any to menuicon navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -130,7 +130,7 @@ export default function NavBar() {
     )
 }
 
-function MenuIcon(props) {
+function MenuIcon(props:any) {
     return (
         <svg
             {...props}
@@ -147,26 +147,6 @@ function MenuIcon(props) {
             <line x1="4" x2="20" y1="12" y2="12" />
             <line x1="4" x2="20" y1="6" y2="6" />
             <line x1="4" x2="20" y1="18" y2="18" />
-        </svg>
-    )
-}
-
-
-function MountainIcon(props) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
         </svg>
     )
 }
